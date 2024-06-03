62667c4ba1c21b26ea56446e4e0e014b200c62bc
Teodorabosak <teodora.bosak@gmail.com>
2024-06-03 18:14:42 +0200

    Add project files.

---
+++ Forme/Korisnik.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTravel.Forme
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	
+	public partial class Korisnik : Window
+	{
+		SqlConnection konekcija = new SqlConnection();
+		Konekcija kon = new Konekcija();
+		
+		
+		public Korisnik()
+		{
+			InitializeComponent();
+			txtIme.Focus();
+			konekcija = kon.KreirajKonekciju();
+		}
+
+
+		private void Button_Click(object sender, RoutedEventArgs e) //sacuvaj
+		{
+
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e) //otkazi
+		{
+			this.Close();
+		}
+	}
+}
---
+++ Konekcija.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTravel
+{
+	class Konekcija
+	{
+		//predstavlja konekciju sa SQL Server bazom
+		public SqlConnection KreirajKonekciju()
+		{
+			SqlConnectionStringBuilder ccnSb = new SqlConnectionStringBuilder();
+			ccnSb.DataSource = @"DESKTOP-COA51EH"; //server
+			ccnSb.InitialCatalog = "TravelWeb"; //naziv baze
+			ccnSb.IntegratedSecurity = true ; //lokalna baza
+			
+			string con = ccnSb.ToString();
+			SqlConnection konekcija = new SqlConnection(con);
+
+			return konekcija;
+		}
+	}
+}
